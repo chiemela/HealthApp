d589964caff3b6ac36864f338981d616d308618e
Chiemela <Chiemela242@gmail.com>
2023-03-26 17:45:27 +0100

    Updates

--- app/src/main/java/com/eliamtechnologies/healthapp/HealthArticlesActivity.java
+++ app/src/main/java/com/eliamtechnologies/healthapp/HealthArticlesActivity.java
@@ -2,13 +2,62 @@ package com.eliamtechnologies.healthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HealthArticlesActivity extends AppCompatActivity {
+
+    private String[][] health_details = {
+            {"Balking Daily", "", "", "", "Click More Details"},
+            {"Home care for COVID-19", "", "", "", "Click More Details"},
+            {"Stop Smoking", "", "", "", "Click More Details"},
+            {"Menstrual Cramps", "", "", "", "Click More Details"},
+            {"Healthy Gut", "", "", "", "Click More Details"},
+    };
+
+    private int[] images = {
+            R.drawable.health1,
+            R.drawable.health2,
+            R.drawable.health3,
+            R.drawable.health4,
+            R.drawable.health5
+    };
+
+    HashMap<String, String> item;
+    ArrayList list;
+    SimpleAdapter sa;
+    Button btnBack;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_articles);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HealthArticlesActivity.this, HomeActivity.class));
+            }
+        });
+
+        // this loops through the Array Objects and puts them in a HashMap
+        list = new ArrayList();
+        for(int i = 0; i < health_details.length; i++){
+            item = new HashMap<String, String>();
+            item.put("line1", health_details[i][0]);
+            item.put("line2", health_details[i][1]);
+            item.put("line3", health_details[i][2]);
+            item.put("line4", health_details[i][3]);
+            item.put("line5", health_details[i][3]);
+            list.add(item);
+        }
     }
 }
