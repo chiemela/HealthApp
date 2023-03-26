b338a0ae19cbf8e0b5b24b20541aa31359f685a3
Chiemela <Chiemela242@gmail.com>
2023-03-26 06:07:05 +0100

    OrderDetailsActivity completed

--- app/src/main/java/com/eliamtechnologies/healthapp/OrderDetailsActivity.java
+++ app/src/main/java/com/eliamtechnologies/healthapp/OrderDetailsActivity.java
@@ -46,6 +46,39 @@ public class OrderDetailsActivity extends AppCompatActivity {
         ArrayList dbData = db.getOderData(username);
 
         // split the string result from database and store in String array "order_details"
-        
+        order_details = new String[dbData.size()][];
+        for(int i = 0; i < order_details.length; i++){
+            order_details[i] = new String[5];   // 5 columns because of the multi_line.xml layout
+            String arrData = dbData.get(i).toString();
+            String[] strData = arrData.split(java.util.regex.Pattern.quote("$"));
+            order_details[i][0] = strData[0];
+            order_details[i][1] = strData[1];
+            if(strData[7].compareTo("medicine") == 0){
+                order_details[i][3] = "Del:" + strData[4];
+            }else{
+                order_details[i][3] = "Del:" + strData[4] + " " + strData[5];
+            }
+            order_details[i][2] = "£ " + strData[6];
+            order_details[i][4] = strData[7];
+        }
+
+        // map each item in "order_details" to "list" ArrayList
+        list = new ArrayList();
+        for(int i = 0; i < order_details.length; i++){
+            item = new HashMap<String, String>();
+            item.put("line1", order_details[i][0]);
+            item.put("line2", order_details[i][1]);
+            item.put("line3", order_details[i][2]);
+            item.put("line4", order_details[i][3]);
+            item.put("line5", order_details[i][4]);
+            list.add(item);
+        }
+
+        // add "list" ArrayList to SimpleAdapter
+        sa = new SimpleAdapter(this, list,
+                R.layout.multi_lines,
+                new String[]{"line1","line2","line3","line4","line5"},
+                new int[]{R.id.line_a,R.id.line_b,R.id.line_c,R.id.line_d,R.id.line_e});
+        lst.setAdapter(sa);
     }
 }
